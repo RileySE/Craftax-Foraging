5440b4ba2ce0a0ff7502eaa0adecb8c9bf27572a
Raymond Chua <ray.r.chua@gmail.com>
2024-07-16 09:43:24 -0400

    Add disable wandb if not used.

--- craftax/ppo_rnn.py
+++ craftax/ppo_rnn.py
@@ -638,6 +638,9 @@ def run_ppo(config):
             + "M",
         )
 
+    else:
+        wandb.init(mode="disabled")
+
     rng = jax.random.PRNGKey(config["SEED"])
     rngs = jax.random.split(rng, config["NUM_REPEATS"])
 
