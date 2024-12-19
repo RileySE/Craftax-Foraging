c5274e52708345ac328783491390a3f21cd16396
lungerjo <j.lunger@mail.utoronto.ca>
2024-12-19 12:58:12 -0500

    policy loading/saving path update

--- craftax/logz/batch_logging.py
+++ craftax/logz/batch_logging.py
@@ -101,7 +101,6 @@ def batch_log(update_step, log, config):
                 )
                 sps = steps_between_updates / dt
                 agg_logs["sps"] = sps
-
         wandb.log(agg_logs)
 
 
