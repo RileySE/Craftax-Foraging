23101172182acbc2d77f2868770f7e2e04c786f0
Riley <noyal@comcast.net>
2025-04-07 18:21:56 -0400

    Removed weird unused import that was causing errors

--- craftax/craftax/constants.py
+++ craftax/craftax/constants.py
@@ -6,7 +6,6 @@ import jax.numpy as jnp
 import imageio.v3 as iio
 import numpy as np
 from PIL import Image
-from sympy import ceiling
 
 from craftax.craftax.util.maths_utils import get_distance_map
 from craftax.environment_base.util import load_compressed_pickle, save_compressed_pickle
