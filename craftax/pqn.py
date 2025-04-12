ed461a256f32e442a0982b3b7db9f0057d99820b
Raymond Chua <ray.r.chua@gmail.com>
2025-04-11 22:20:43 -0400

    Return metrics from runner state in pqn.py

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -674,7 +674,7 @@ def make_train(config):
                 update_step + 1,
             )
 
-            return runner_state, None
+            return runner_state, metrics
 
         def get_test_metrics(train_state, rng):
 
