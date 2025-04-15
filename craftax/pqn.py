9baad3b2a19af7388fba7d06048ce012ae8329f4
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 19:45:28 -0400

    Rename 'q_vals' to 'value' in info dictionary for clarity in pqn.py

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -740,7 +740,7 @@ def make_train(config):
             q_vals_action_taken = jnp.take_along_axis(q_vals, jnp.expand_dims(new_action, axis=-1), axis=-1).squeeze(axis=-1)
             print("q_vals_action_taken shape", q_vals_action_taken.shape)
 
-            info['q_vals'] = q_vals_action_taken
+            info['value'] = q_vals_action_taken
             info['pred_delta'] = aux
             info['delta'] = deltas_to_start
 
