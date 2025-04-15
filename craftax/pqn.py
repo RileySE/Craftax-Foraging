6eb588ff769678b557f2e684a79e79d3318d79c9
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 18:42:46 -0400

    Implement save_weights_callback function in pqn.py for logging model weights

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -879,19 +879,19 @@ def make_train(config):
             )
 
             # Log model weights
-            # def save_weights_callback(weights_flat, iter):
-            #     run_out_path = os.path.join(config['OUTPUT_PATH'], wandb.run.id)
-            #     os.makedirs(run_out_path, exist_ok=True)
-            #     weight_filename = os.path.join(run_out_path, 'weights_{}.csv'.format(iter))
-            #     weight_file = open(weight_filename, 'w')
-            #     for weights_set in weights_flat:
-            #         if len(weights_set.shape) == 1:
-            #             continue
-            #         np.savetxt(weight_file, np.transpose(weights_set), delimiter=',', fmt='%f')
-            #     print('Saving weights in file', weight_filename)
-            #
-            # weights_flat = jax.tree.flatten(runner_state[0].params)
-            # jax.debug.callback(save_weights_callback, weights_flat[0], runner_state[-1])
+            def save_weights_callback(weights_flat, iter):
+                run_out_path = os.path.join(config['OUTPUT_PATH'], wandb.run.id)
+                os.makedirs(run_out_path, exist_ok=True)
+                weight_filename = os.path.join(run_out_path, 'weights_{}.csv'.format(iter))
+                weight_file = open(weight_filename, 'w')
+                for weights_set in weights_flat:
+                    if len(weights_set.shape) == 1:
+                        continue
+                    np.savetxt(weight_file, np.transpose(weights_set), delimiter=',', fmt='%f')
+                print('Saving weights in file', weight_filename)
+
+            weights_flat = jax.tree.flatten(runner_state[0].params)
+            jax.debug.callback(save_weights_callback, weights_flat[0], runner_state.n_updates)
 
             # Can we save the environment state and resume training later?
             # runner_state_copy = runner_state
