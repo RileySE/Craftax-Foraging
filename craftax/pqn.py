428bb38b1d6e17a9ad5c5453e0c7faa4c0029c64
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 18:26:05 -0400

    Update training configuration calculations and correct runner state update function

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -245,11 +245,11 @@ class CustomTrainState(TrainState):
 
 def make_train(config):
     config["NUM_UPDATES"] = (
-            config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"]
+            config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"] // config['UPDATES_PER_VIZ']
     )
 
     config["NUM_UPDATES_DECAY"] = (
-            config["TOTAL_TIMESTEPS_DECAY"] // config["NUM_STEPS"] // config["NUM_ENVS"]
+            config["TOTAL_TIMESTEPS_DECAY"] // config["NUM_STEPS"] // config["NUM_ENVS"] // config['UPDATES_PER_VIZ']
     )
 
     assert (config["NUM_STEPS"] * config["NUM_ENVS"]) % config[
@@ -914,13 +914,13 @@ def make_train(config):
         rng, _rng = jax.random.split(rng)
         runner_state = (train_state, expl_state, test_metrics, _rng)
 
-        runner_state, metrics = jax.lax.scan(
-            _update_step, runner_state, None, config["NUM_UPDATES"]
-        )
-
         # runner_state, metrics = jax.lax.scan(
-        #     _update_plot, runner_state, None, config["NUM_UPDATES"]
+        #     _update_step, runner_state, None, config["NUM_UPDATES"]
         # )
+
+        runner_state, metrics = jax.lax.scan(
+            _update_plot, runner_state, None, config["NUM_UPDATES"]
+        )
 
         return {"runner_state": runner_state, "metrics": metrics}
 
