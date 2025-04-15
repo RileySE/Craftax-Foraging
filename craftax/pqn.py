ca71e3128b139afef0839f4620da2c1b7642fe14
Raymond Chua <ray.r.chua@gmail.com>
2025-04-15 19:46:27 -0400

    Update debug print statement to reflect 'value' key in info dictionary in pqn.py

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -744,7 +744,7 @@ def make_train(config):
             info['pred_delta'] = aux
             info['delta'] = deltas_to_start
 
-            print("info['q_vals'] shape", info['q_vals'].shape)
+            print("info['value'] shape", info['value'].shape)
             print("info['pred_delta'] shape", info['pred_delta'].shape)
             print("info['delta'] shape", info['delta'].shape)
 
