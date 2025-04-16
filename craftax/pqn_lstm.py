38c1592803ded448f169b88add08acb0b78368a7
Raymond Chua <ray.r.chua@gmail.com>
2025-04-16 14:48:31 -0400

    Remove unused variable from LSTM state unpacking for cleaner code

--- craftax/pqn_lstm.py
+++ craftax/pqn_lstm.py
@@ -821,7 +821,7 @@ def make_train(config):
         def _env_step_viz(runner_state, unused):
             # train_state, expl_state, test_metrics, rng = runner_state
             train_state, memory_transitions, expl_state, test_metrics, rng = runner_state
-            hs, last_obs, last_done, last_action, env_state, rng = expl_state
+            hs, last_obs, last_done, last_action, env_state = expl_state
             rng, rng_a  = jax.random.split(rng)
 
             _obs = last_obs[np.newaxis]  # (1 (dummy time), num_envs, obs_size)
