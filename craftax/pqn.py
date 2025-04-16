1cd79fa4cf94afd3c7a1ce13429c437146677735
Raymond Chua <ray.r.chua@gmail.com>
2025-04-16 13:20:53 -0400

    Remove print statements for metric values in callback function

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -650,9 +650,6 @@ def make_train(config):
                     to_log = create_log_dict(metrics, config)
                     batch_log(metrics["update_steps"], to_log, config)
 
-                    for k, v in metrics.items():
-                        print(f"{k}: {v}")
-
                 jax.debug.callback(callback, metrics, original_rng)
 
             runner_state = (train_state, tuple(expl_state), test_metrics, rng)
