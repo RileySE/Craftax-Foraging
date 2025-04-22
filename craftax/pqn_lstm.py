25c070f1712ab95314765600e525ff881db7c879
Raymond Chua <ray.r.chua@gmail.com>
2025-04-22 11:17:51 -0400

    Refactor pqn_lstm.py to unpack hstate for clearer variable usage

--- craftax/pqn_lstm.py
+++ craftax/pqn_lstm.py
@@ -918,7 +918,8 @@ def make_train(config):
             # Callback function for logging the scalars
             def write_rnn_hstate(hstate, scalars, increment=0):
 
-                cell_state, hidden_state = hstate
+                cell_state = hstate[0]
+                hidden_state = hstate[1]
                 print("cell_state shape: ", cell_state.shape)
                 print("hidden_state shape: ", hidden_state.shape)
 
