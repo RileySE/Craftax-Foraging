76c033db769f78224a70715f265d491f61ae39c5
Raymond Chua <ray.r.chua@gmail.com>
2025-04-22 11:01:38 -0400

    Remove debug print statement for logging threads in ppo_rnn.py

--- craftax/ppo_rnn.py
+++ craftax/ppo_rnn.py
@@ -721,7 +721,6 @@ def make_train(config):
                     scalar_file_header += ',' + key
 
                 # We save to temp files and then append to the target file since numpy apparently cannot write files in append mode for some reason
-                print("logging threads", logging_threads)
                 for i in range(logging_threads):
                     out_filename_hstates = os.path.join(run_out_path, 'hstates_{}_{}.csv'.format(increment, i))
                     temp_filename = os.path.join(run_out_path, 'temp.csv')
