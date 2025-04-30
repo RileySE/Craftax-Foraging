f8cbf6a2cf0181bc082fc704665c5d8bfe0439b2
Raymond Chua <ray.r.chua@gmail.com>
2025-04-30 11:53:08 -0400

    Update pqn_lstm.py to log metrics using to_log for improved clarity

--- craftax/pqn_lstm.py
+++ craftax/pqn_lstm.py
@@ -678,8 +678,8 @@ def make_train(config):
                     to_log = create_log_dict(metrics, config)
                     batch_log(metrics["update_steps"], to_log, config)
 
-                    # for k,v in metrics.items():
-                    #     print(f"{k}: {v}")
+                    for k,v in to_log.items():
+                        print(f"{k}: {v}")
 
                 jax.debug.callback(callback, to_log, original_rng)
 
