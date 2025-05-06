600a22dc361272b55d1c56f261ff91763dd53510
Raymond Chua <ray.r.chua@gmail.com>
2025-05-05 22:15:16 -0400

    Remove commented-out UPDATES_PER_VIZ line in pqn.py for cleaner code

--- craftax/pqn.py
+++ craftax/pqn.py
@@ -373,7 +373,6 @@ def make_train(config):
         config["TOTAL_TIMESTEPS"]
         // config["NUM_STEPS"]
         // config["NUM_ENVS"]
-        // config["UPDATES_PER_VIZ"]
     )
 
     config["NUM_UPDATES_DECAY"] = (
@@ -383,6 +382,7 @@ def make_train(config):
     assert (config["NUM_STEPS"] * config["NUM_ENVS"]) % config[
         "NUM_MINIBATCHES"
     ] == 0, "NUM_MINIBATCHES must divide NUM_STEPS*NUM_ENVS"
+
 
     config["NUM_LOG_STEPS"] = config["NUM_UPDATES"] * config["UPDATES_PER_VIZ"]
 
